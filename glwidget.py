253501309f05e84baf66552ceecf31bcd4ba2e73
g0c0de0rd1e <kqlykjk@gmail.com>
2024-11-15 18:32:26 +0300

    added rotation and moving

--- glwidget.py
+++ glwidget.py
@@ -1,7 +1,5 @@
-# glwidget.py
-
 from PyQt5.QtWidgets import QOpenGLWidget
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from shapes import Cube
@@ -9,19 +7,15 @@ from shapes import Cube
 class GLWidget(QOpenGLWidget):
     def __init__(self, parent=None):
         super(GLWidget, self).__init__(parent)
-        self.rotation_angle = 0
         self.cube = Cube()  # Инициализация куба
+        self.last_pos = QPoint()
+        self.rotation = [0, 0, 0]
+        self.scale = 1.0
+        self.translation = [0.0, 0.0]
 
     def initializeGL(self):
         glClearColor(0.0, 0.0, 0.0, 1.0)
         glEnable(GL_DEPTH_TEST)
-
-        # Настройка перспективы
-        glMatrixMode(GL_PROJECTION)
-        gluPerspective(45.0, 1.0, 0.1, 100.0)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-        gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0)
 
     def resizeGL(self, w, h):
         glViewport(0, 0, w, h)
@@ -29,18 +23,38 @@ class GLWidget(QOpenGLWidget):
         glLoadIdentity()
         gluPerspective(45.0, w / h, 0.1, 100.0)
         glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0)
 
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
-        glTranslatef(0.0, 0.0, -5.0)
-        glRotatef(self.rotation_angle, 1.0, 1.0, 1.0)
+        glTranslatef(self.translation[0], self.translation[1], -5.0)
+        glScalef(self.scale, self.scale, self.scale)
+        glRotatef(self.rotation[0], 1.0, 0.0, 0.0)
+        glRotatef(self.rotation[1], 0.0, 1.0, 0.0)
+        glRotatef(self.rotation[2], 0.0, 0.0, 1.0)
         self.cube.draw()  # Рендеринг куба
 
-    def rotate_object(self):
-        self.rotation_angle += 1.0
+    def mousePressEvent(self, event):
+        self.last_pos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        dx = event.x() - self.last_pos.x()
+        dy = event.y() - self.last_pos.y()
+
+        if event.buttons() & Qt.LeftButton:
+            self.rotation[0] += dy
+            self.rotation[1] += dx
+        elif event.buttons() & Qt.RightButton:
+            self.translation[0] += dx / 100.0
+            self.translation[1] -= dy / 100.0
+
+        self.last_pos = event.pos()
         self.update()
 
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_R:
-            self.rotate_object()
+    def wheelEvent(self, event):
+        delta = event.angleDelta().y() / 120  # Количество щелчков колесика мыши
+        self.scale += delta * 0.1
+        self.scale = max(0.1, self.scale)  # Минимальный масштаб
+        self.update()
